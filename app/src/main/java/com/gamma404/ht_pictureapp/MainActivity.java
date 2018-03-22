d9c9caa5ea011f946d73beea0c4554af81c93749
AM903103 <33500492+AM903103@users.noreply.github.com>
2018-03-22 22:37:15 +0800

    SimpleCursorAdapter, CursorLoader, MediaStore

--- app/src/main/java/com/gamma404/ht_pictureapp/MainActivity.java
+++ app/src/main/java/com/gamma404/ht_pictureapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.gamma404.ht_pictureapp;
 
 import android.Manifest;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -10,13 +15,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.GridView;
+import android.widget.SimpleCursorAdapter;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int REQUEST_READ_STORAGE = 3;
+    private SimpleCursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +48,38 @@ public class MainActivity extends AppCompatActivity {
             new AlertDialog.Builder(this)
                     .setTitle("Read Storage")
                     .setMessage("要先取得外部權限 才能顯示圖檔")
-                    .setPositiveButton("OK",null)
+                    .setPositiveButton("OK", null)
                     .show();
         }
     }
 
     private void readThumbnails() {
         Log.d(TAG, "readThumbnails: ");
+        GridView gridView = findViewById(R.id.grid);
+        String[] from = {MediaStore.Images.Thumbnails.DATA,
+                MediaStore.Images.Media.DISPLAY_NAME};
+        int[] to = new int[]{R.id.thumb_image, R.id.thumb_text};
+        adapter = new SimpleCursorAdapter(this,
+                R.layout.thumb_item, null, from, to, 0);
+        gridView.setAdapter(adapter);
+        getLoaderManager().initLoader(3, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        return new CursorLoader(this, uri,
+                null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        adapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
