264bf3dd2865fc520c60837e60aea2d159c6f0dd
AM903103 <33500492+AM903103@users.noreply.github.com>
2018-03-26 19:42:16 +0800

    Gesture

--- app/src/main/java/com/gamma404/ht_pictureapp/DetailActivity.java
+++ app/src/main/java/com/gamma404/ht_pictureapp/DetailActivity.java
@@ -7,17 +7,22 @@ import android.graphics.BitmapFactory;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
     private Cursor cursor;
     private ImageView imageView;
+    private GestureDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        detector = new GestureDetector(this, this);
 
         int position = getIntent().getIntExtra("POSITION", 0);
         imageView = findViewById(R.id.thumb_image);
@@ -37,5 +42,57 @@ public class DetailActivity extends AppCompatActivity {
                 cursor.getColumnIndex(MediaStore.Images.Media.DATA));
         Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
         imageView.setImageBitmap(bitmap);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+//        return super.onTouchEvent(event);
+        //改由GestureDetector處理Activity會自動執行的onTouchEvent
+        //未來手機上螢幕的操作會全部改由GestureDetector物件處理
+        return detector.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        float distance = e2.getX() - e1.getX();
+        if (distance > 100) {//左往右滑 正數
+            //向右 往前一張圖
+            if (!cursor.moveToPrevious()) {
+                cursor.moveToLast();
+            }
+            updateImage();
+        } else if (distance < -100) {//右往左滑 正數
+            //向左 往後一張圖
+            if (!cursor.moveToNext()) {
+                cursor.moveToFirst();
+            }
+            updateImage();
+        }
+        return false;
     }
 }
