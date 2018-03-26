178787060b99d54399ad001100f60cc685dfc889
AM903103 <33500492+AM903103@users.noreply.github.com>
2018-03-26 19:27:34 +0800

    Click item enter Detail

--- app/src/main/java/com/gamma404/ht_pictureapp/DetailActivity.java
+++ app/src/main/java/com/gamma404/ht_pictureapp/DetailActivity.java
@@ -1,13 +1,41 @@
 package com.gamma404.ht_pictureapp;
 
+import android.content.CursorLoader;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private Cursor cursor;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        int position = getIntent().getIntExtra("POSITION", 0);
+        imageView = findViewById(R.id.thumb_image);
+
+        CursorLoader loader = new CursorLoader(this,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, null, null, null);
+
+        cursor = loader.loadInBackground();
+        cursor.moveToPosition(position);
+
+        updateImage();
+    }
+
+    private void updateImage() {
+        String imagePath = cursor.getString(
+                cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+        imageView.setImageBitmap(bitmap);
     }
 }
